31728c89c525344764396e164e425d0418c1da55
Abhishek Singh <abhisheksingh050530@gmail.com>
2025-06-24 19:51:29 +0530

    data validation complete

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-
-
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
-STAGE_NAME = "Data Ingestion Stage"
+STAGE_NAME_1 = "Data Ingestion Stage"
+STAGE_NAME_2 = "Data Validation Stage"
 
 if __name__ == "__main__":
     try:
-        logger.info(f">>>> Stage '{STAGE_NAME}' started <<<<")
+        logger.info(f">>>> Stage '{STAGE_NAME_1}' started <<<<")
         data_ingestion = DataIngestionTrainingPipeline()
         data_ingestion.main()
-        logger.info(f">>>> Stage '{STAGE_NAME}' completed <<<<\n\nX================X\n")
+        logger.info(f">>>> Stage '{STAGE_NAME_1}' completed <<<<\n\nX================X\n")
+
+        logger.info(f">>>> Stage '{STAGE_NAME_2}' started <<<<")
+        data_validation = DataValidationTrainingPipeline()
+        data_validation.main()
+        logger.info(f">>>> Stage '{STAGE_NAME_2}' completed <<<<\n\nX================X\n")
+
     except Exception as e:
         logger.exception(e)
         raise e
