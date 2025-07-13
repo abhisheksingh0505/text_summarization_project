8a8d05340cdc6c7ea507f19058b51293b253cb1b
Abhishek Singh <abhisheksingh050530@gmail.com>
2025-07-13 15:56:40 +0530

    model evaluation done

--- src/textSummarizer/pipeline/stage_04_model_trainer.py
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -8,7 +8,7 @@ class ModelTrainerTrainingPipeline:
         pass
 
     def main(self):
-       config = ConfigurationManager()
-       model_trainer_config = config.get_model_trainer_config()
-       model_trainer_config = ModelTrainer(config=model_trainer_config)
-       model_trainer_config.train()  
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
